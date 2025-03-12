730e2755c7d52dcefbb84599bcc3a431701b62cb
Gokul-sami <gokulpandiyan6@gmail.com>
2025-03-12 21:32:50 +0530

    added env file

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,16 +1,25 @@
 import { initializeApp } from 'firebase/app';
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { 
+  FIREBASE_API_KEY, 
+  FIREBASE_AUTH_DOMAIN, 
+  FIREBASE_PROJECT_ID, 
+  FIREBASE_STORAGE_BUCKET, 
+  FIREBASE_MESSAGING_SENDER_ID, 
+  FIREBASE_APP_ID, 
+  FIREBASE_MEASUREMENT_ID 
+} from '@env';
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBAMylU_7q4OflHh-3952kg_lsabap8XRs",
-  authDomain: "mystudyplan-73214.firebaseapp.com",
-  projectId: "mystudyplan-73214",
-  storageBucket: "mystudyplan-73214.firebasestorage.app",
-  messagingSenderId: "1001491650670",
-  appId: "1:1001491650670:web:dca87e08309ada7ad0ee7f",
-  measurementId: "G-9PGLMGT8WD"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
